6dad8e0c15e8d4f7f4083c6d1034e9a23c7ece6f
pushparajkum <kumbharkar.pushparaj@gmail.com>
2019-01-29 23:22:05 +0530

    lecture 27jan

---
+++ lecture_27jan/prime_number.py
@@ -0,0 +1,20 @@
+def prime(n, flag):
+	if n%2 == 0:
+		return False
+
+	for x in range(3, n//2+1, 2):
+		if n%x == 0:
+			flag = 0
+			print("The number is not prime..")
+			return False
+	
+	if flag == 1:
+		print("The number is prime..")
+
+def main():
+	flag = 1
+	n = eval(input("Enter a number :: "))
+	prime(n, flag)
+
+if __name__ == '__main__':
+	main()
