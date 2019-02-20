b698d5a9a685fbe563607e443439519f662c762d
pushparajkum <kumbharkar.pushparaj@gmail.com>
2019-02-20 22:32:42 +0530

    bubble sort

--- lecture_16feb/bubble_sort.py
+++ lecture_16feb/bubble_sort.py
@@ -1,7 +1,6 @@
 
 def bubble_sort(num_list):
 	for i in range(0, len(num_list) - 1):
-		already_sorted = True
 		for j in range(0, len(num_list) - i - 1):
 			if num_list[j] > num_list[j+1]:
 				x = num_list[j]
@@ -9,11 +8,13 @@ def bubble_sort(num_list):
 				num_list[j+1] = x
 			else:
 				already_sorted = False
-		
+
+	return num_list
 
 def main():
 	num_list = eval(input("Enter the list of numbers to sort : "))
-	bubble_sort(num_list)
+	res = bubble_sort(num_list)
+	print(res)
 
 if __name__ == "__main__":
 	main()
